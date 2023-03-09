441a7a8f79a7a0a466cf2a6aff7f88e9a5d2247e
Mohsen Bakhshandeh <waltonmohsen@gmail.com>
2023-03-10 03:12:41 +0330

    added bin folder for seprating Binaries move code from main to /bin/guess_the_number.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-use rand::Rng;
-use std::{cmp::Ordering, io::stdin, u32};
-
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        let mut guess: String = String::new();
-
-        stdin().read_line(&mut guess).expect("Error Reading Input.");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Enter number not strings 😡, Write the number you guess:");
-                continue;
-            }
-        };
-
-        println!("Write the number you guess:");
-        match guess.cmp(&secret_number) {
-            Ordering::Equal => {
-                println!("You are right 🫡");
-                break;
-            }
-            Ordering::Greater => println!("Too big 😲"),
-            Ordering::Less => println!("Too Smoll 😒"),
-        }
-    }
+    println!("Nothing to see here 🤔");
 }
